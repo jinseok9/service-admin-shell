8b32d6d546139d728ef3eb6a7585f67d15cc6278
jinseok <jinseok@jobis.co>
2024-12-11 06:31:12 +0900

    feat: shared 맞게 설정

---
+++ src/index.d.ts
@@ -0,0 +1,3 @@
+declare module "service-admin-iit/IIT" {
+  export default function (): JSX.Element;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
-import { StrictMode } from "react";
+import { StrictMode, Suspense } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Suspense fallback={<div>Loading Shell...</div>}>
+        <App />
+      </Suspense>
+    </BrowserRouter>
   </StrictMode>
 );
